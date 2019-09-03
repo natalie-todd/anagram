0bb396f23809c352af6f60e5139ebbbc82356002
natalie-todd <natalie.todd15@gmail.com>
2019-09-03 07:35:38 -0600

    Configure Swagger

--- src/main/java/com/ibotta/anagram/AnagramApplication.java
+++ src/main/java/com/ibotta/anagram/AnagramApplication.java
@@ -20,6 +20,6 @@ public class AnagramApplication {
 	@Bean
 	public Docket productApi() {
 		return new Docket(DocumentationType.SWAGGER_2).select()
-				.apis(RequestHandlerSelectors.basePackage("com.tutorialspoint.swaggerdemo")).build();
+				.apis(RequestHandlerSelectors.basePackage("com.ibotta.anagram")).build();
 	}
 }
--- src/main/java/com/ibotta/anagram/controller/WordsController.java
+++ src/main/java/com/ibotta/anagram/controller/WordsController.java
@@ -5,17 +5,20 @@ import com.ibotta.anagram.model.AnagramsFoundResponse;
 import com.ibotta.anagram.model.CountResponse;
 import com.ibotta.anagram.model.GroupResponse;
 import com.ibotta.anagram.service.AnagramService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
 
-@org.springframework.stereotype.Controller
-@Validated
+@RestController
+@Api(value = "/anagrams", description = "Evaluate anagrams within dictionary")
 public class WordsController {
     private AnagramService anagramService;
 
@@ -28,6 +31,15 @@ public class WordsController {
 
     @CrossOrigin(origins = "*")
     @RequestMapping(value = "/words.json", produces = { MediaType.APPLICATION_JSON_VALUE }, method = RequestMethod.POST)
+    @ResponseBody
+    @ApiOperation(value = "Add anagrams",
+            httpMethod = "POST",
+            produces = "application/json",
+            consumes = "application/json",
+            response = ResponseEntity.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 400, message = "Invalid anagram request")
+    })
     public ResponseEntity<Void> addWords(@Valid @RequestBody AddWordsRequest request) {
     ResponseEntity response = anagramService.addWords(request);
         System.out.println("----->"+response);
@@ -36,6 +48,16 @@ public class WordsController {
 
     @CrossOrigin(origins = "*")
     @RequestMapping(value = "/anagrams/{word}.json", produces = { MediaType.APPLICATION_JSON_VALUE }, method = RequestMethod.GET)
+    @ResponseBody
+    @ApiOperation(value = "Find anagrams",
+            httpMethod = "GET",
+            produces = "application/json",
+            consumes = "application/json",
+            response = AnagramsFoundResponse.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully found anagrams."),
+            @ApiResponse(code = 400, message = "Invalid anagram request")
+    })
     public ResponseEntity<AnagramsFoundResponse> findAnagrams(
             @PathVariable("word") String word,
             @RequestParam(required = false) Integer limit
@@ -47,6 +69,15 @@ public class WordsController {
 
     @CrossOrigin(origins = "*")
     @RequestMapping(value = "/words/{word}.json", produces = { MediaType.APPLICATION_JSON_VALUE }, method = RequestMethod.DELETE)
+    @ResponseBody
+    @ApiOperation(value = "Delete anagram",
+            httpMethod = "DELETE",
+            produces = "application/json",
+            consumes = "application/json",
+            response = ResponseEntity.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 400, message = "Invalid anagram request")
+    })
     public ResponseEntity<Void> deleteWord(
             @PathVariable("word") String word
     ) {
@@ -57,6 +88,15 @@ public class WordsController {
 
     @CrossOrigin(origins = "*")
     @RequestMapping(value = "/words.json", produces = { MediaType.APPLICATION_JSON_VALUE }, method = RequestMethod.DELETE)
+    @ResponseBody
+    @ApiOperation(value = "Delete all anagrams",
+            httpMethod = "DELETE",
+            produces = "application/json",
+            consumes = "application/json",
+            response = ResponseEntity.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 400, message = "Invalid anagram request")
+    })
     public ResponseEntity<Void> deleteAll() {
         ResponseEntity<Void> response = anagramService.deleteAll();
 
@@ -65,6 +105,16 @@ public class WordsController {
 
     @CrossOrigin(origins = "*")
     @RequestMapping(value = "/count.json", produces = { MediaType.APPLICATION_JSON_VALUE }, method = RequestMethod.GET)
+    @ResponseBody
+    @ApiOperation(value = "Get anagram count info",
+            httpMethod = "GET",
+            produces = "application/json",
+            consumes = "application/json",
+            response = CountResponse.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully fetched anagram count info."),
+            @ApiResponse(code = 400, message = "Invalid anagram request")
+    })
     public ResponseEntity<CountResponse> findAnagrams(
     ) {
         CountResponse response = anagramService.countWords();
@@ -74,6 +124,16 @@ public class WordsController {
 
     @CrossOrigin(origins = "*")
     @RequestMapping(value = "/anagramGroup.json", produces = { MediaType.APPLICATION_JSON_VALUE }, method = RequestMethod.GET)
+    @ResponseBody
+    @ApiOperation(value = "Evaluate anagram group",
+            httpMethod = "GET",
+            produces = "application/json",
+            consumes = "application/json",
+            response = GroupResponse.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully deleted anagram."),
+            @ApiResponse(code = 400, message = "Invalid anagram request")
+    })
     public ResponseEntity<GroupResponse> evaluateWords(
             @RequestParam List<String> words
     ) {
@@ -84,6 +144,16 @@ public class WordsController {
 
     @CrossOrigin(origins = "*")
     @RequestMapping(value = "/anagrams/{word}.json", produces = { MediaType.APPLICATION_JSON_VALUE }, method = RequestMethod.DELETE)
+    @ResponseBody
+    @ApiOperation(value = "Delete word and anagrams",
+            httpMethod = "DELETE",
+            produces = "application/json",
+            consumes = "application/json",
+            response = ResponseEntity.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully deleted word and anagrams."),
+            @ApiResponse(code = 400, message = "Invalid anagram request")
+    })
     public ResponseEntity<Void> deleteAnagrams(
             @PathVariable("word") String word
     ) {
